bb5664fdc83871cf1608308fa8f501c8d2afa67f
marc brout <marc.brout@epitech.eu>
2017-03-14 14:54:01 +0100

    RSI, Averages, Eponentials done

--- src/Curve.cpp
+++ src/Curve.cpp
@@ -2,24 +2,43 @@
 // Created by brout_m on 11/03/17.
 //
 
+#include <vector>
+#include <algorithm>
 #include "Curve.hh"
 
 double trade::Curve::getAverage(size_t period) {
-    avg_t::iterator it;
-
-    if ((it = averages.find(period)) == averages.end())
+    if (averages.find(period) == averages.end())
         averages[period] = updateAverage(period);
-    return (it->second);
+    return averages[period];
+}
+
+double trade::Curve::getRSI(size_t period) {
+    if (rsi.find(period) == rsi.end())
+        rsi[period] = updateRSI(period);
+    return rsi[period];
+}
+
+double trade::Curve::getExponential(size_t period, size_t pow) {
+    expo_t::iterator it;
+
+    if ((it = exponentials.find(period)) == exponentials.end())
+    {
+        exponentials[period] = {{pow, updateExponential(period, pow)}};
+    }
+    else
+    {
+        if (it->second.find(pow) == it->second.end())
+            exponentials[period][pow] = updateExponential(period, pow);
+    }
+
+    return exponentials[period][pow];
 }
 
 void trade::Curve::feed(double value) {
     values.push_front(value);
 
-    for(avg_t::iterator it = averages.begin();
-        it != averages.end(); ++it)
-    {
-        it->second =  updateAverage(it->first);
-    }
+    std::for_each(averages.begin(), averages.end(), [](pair_t &avg){ avg.second = updateAverage(avg.first); });
+    std::for_each(rsi.begin(), rsi.end(), [](pair_t &_rsi){ _rsi.second = updateRSI(_rsi.first); });
 }
 
 double trade::Curve::updateAverage(size_t period) {
@@ -31,6 +50,43 @@ double trade::Curve::updateAverage(size_t period) {
     while (count < period && count < values.size())
     {
         avg += values[count];
+        ++count;
+    }
+    return (avg / count);
+}
+
+double trade::Curve::updateRSI(size_t period) {
+    std::vector low;
+    std::vector high;
+    double avg, highAvg, lowAvg;
+    int count;
+
+    count = 0;
+    avg = getAverage(period);
+    while (count < period && count < values.size())
+    {
+        if (values[count] >= avg)
+            low.push_back(values[count]);
+        else
+            high.push_back(values[count]);
+        ++count;
+    }
+    highAvg = lowAvg = 0;
+    std::for_each(low.begin(), low.end(), [&lowAvg](double x){ lowAvg += x; });
+    std::for_each(high.begin(), high.end(), [&highAvg](double x){ highAvg += x; });
+    highAvg /= high.size();
+    lowAvg /= low.size();
+    return (100.0 - (100.0 / (1.0 + (highAvg / lowAvg))));
+}
+
+double trade::Curve::updateExponential(size_t period, size_t pow) {
+    double avg;
+    double count;
+
+    avg = count = 0;
+    while (count < period && count < values.size())
+    {
+        avg += values[count] * (pow / (count + 1));
         ++count;
     }
     return (avg / count);
--- src/Curve.hh
+++ src/Curve.hh
@@ -15,18 +15,29 @@ namespace trade
     {
     public:
         typedef std::map<size_t, double> avg_t;
+        typedef std::pair<size_t, double> pair_t;
+        typedef std::map<size_t, avg_t > expo_t;
 
     public:
         ~Curve(){};
+        Curve(){};
         double getAverage(size_t period);
+        double getRSI(size_t period);
+        double getExponential(size_t period, size_t pow);
         void feed(double value);
 
     private:
         avg_t averages;
+        avg_t rsi;
+        expo_t exponentials;
         std::deque<double> values;
 
     private:
         double updateAverage(size_t period);
+        double updateRSI(size_t period);
+        double updateExponential(size_t period, size_t pow);
+        Curve(Curve const& curve) = delete;
+        Curve &operator=(Curve const&  curve) = delete;
     };
 }
 
